8dcfe8d9b79e47efe721f89d2af4671857a77249
Ariel <arieltorrente4@gmail.com>
2026-09-09 14:33:09 -0300

    integração dos campos restantes do formulario e botões

--- src/app/cliente/cliente.ts
+++ src/app/cliente/cliente.ts
@@ -3,15 +3,25 @@ import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputMaskModule } from 'primeng/inputmask';
+import { SelectModule } from 'primeng/select';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   imports: [
     CardModule,
+    ButtonModule,
     InputTextModule,
     FloatLabelModule,
+    SelectModule,
     InputMaskModule,],
   selector: 'app-cliente',
   styleUrl: './cliente.css',
   templateUrl: './cliente.html',
 })
-export class Cliente {}
+export class Cliente {
+  tipoPessoa: string = 'PF';
+    tipoPessoaOptions: any[] = [
+    { descricao: 'Pessoa Física', valor: 'PF'},
+    { descricao: 'Pessoa Jurídica', valor: 'PJ'}
+  ];
+}

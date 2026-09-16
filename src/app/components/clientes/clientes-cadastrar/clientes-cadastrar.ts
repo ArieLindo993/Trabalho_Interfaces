82f864973a0b5abaf00bbc6a5cc72a4fae46583a
Ariel <arieltorrente4@gmail.com>
2026-09-15 21:51:59 -0300

    configurar formulario de cadastro de cliente

--- src/app/components/clientes/clientes-cadastrar/clientes-cadastrar.ts
+++ src/app/components/clientes/clientes-cadastrar/clientes-cadastrar.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
@@ -10,7 +10,7 @@ import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-clientes-cadastrar',
   imports: [
-    FormsModule,
+    ReactiveFormsModule,
     CardModule,
     ButtonModule,
     InputTextModule,
@@ -21,10 +21,36 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './clientes-cadastrar.css',
   templateUrl: './clientes-cadastrar.html',
 })
+
 export class ClientesCadastrar {
-  tipoPessoa: 'PF' | 'PJ' = 'PF';
+
+  formularioCliente!: FormGroup;
+
+  mensagemValidacao?: string;
+
   tipoPessoaOptions: { descricao: string; valor: 'PF' | 'PJ' }[] = [
     { descricao: 'Pessoa Física', valor: 'PF' },
     { descricao: 'Pessoa Jurídica', valor: 'PJ' },
   ];
+
+  constructor( private readonly criadorFormulario: FormBuilder) {}
+
+  ngOnInit(): void{
+    this.criarFormularioCliente();
+  }
+
+  criarFormularioCliente(){
+    this.formularioCliente = this.criadorFormulario.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(50)])],
+      tipoPessoa: ['PF', Validators.required],
+      cpfCnpj: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', Validators.required],
+      logradouro: ['', Validators.required],
+      numero: ['', Validators.required],
+      bairro: ['', Validators.required],
+      cep: ['', Validators.required],
+      cidade: ['', Validators.required]
+    })
+  }
 }
